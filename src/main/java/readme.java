c3842986429a9b5ef66ed40d78ea3c0173cab921
zhangling <390597591@qq.com>
2021-12-10 14:59:06 +0800

    【feat】添加双缓冲，解决界面闪烁问题

--- src/main/java/readme.java
+++ src/main/java/readme.java
@@ -3,7 +3,8 @@
  * 3. 抽象出坦克类，并封装相应的属性 {@link com.ling.tank.Tank}
  * 4. 坦克增加静止属性
  * 5. 添加子弹类，并在界面上绘制出来 {@link com.ling.tank.Bullet}
- * 7. 按 ctrl 发射子弹，可以有多个（子弹使用增强 for 循环会报错） {@link com.ling.tank.Tank#fire()}
+ * 6. 添加双缓冲，解决界面闪烁问题  {@link com.ling.tank.TankFrame#update(java.awt.Graphics)}
+ * 7. 按 ctrl 发射子弹，可以有多个 {@link com.ling.tank.Tank#fire()}
  * 8. 解决子弹不断增加存在内存泄漏的问题（超过边框从集合中移除）集合为空时报错
  * 9. 读取图片资源  {@link ImageTest#test()}
  * 10. 将坦克，子弹用图片替换（读取图片，直接画图片），修改子弹到正确位置，相对于最左上角的相对位置
